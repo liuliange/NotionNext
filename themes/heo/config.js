77986a06fcf684be1321a4410e7e3accafcb9305
榴莲哥 <949995568@qq.com>
2026-06-14 20:36:39 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://199909.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://199909.com' }
+    { title: '快捷指令', url: 'https://www.binguo.net' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -20,7 +20,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'https://www.binguo.ne',
+  HEO_HERO_TITLE_3: '199909.com',
   HEO_HERO_TITLE_4: '缤果',
   HEO_HERO_TITLE_5: '缤果',
   HEO_HERO_TITLE_LINK: 'https://199909.com',
@@ -54,7 +54,7 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://www.binguo.ne',
+  HEO_INFO_CARD_URL2: 'https://www.binguo.net',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
   HEO_INFO_CARD_URL3: 'https://www.binguo.net',
   HEO_INFO_CARD_TEXT3: '了解更多',
@@ -123,7 +123,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://jq.qq.com/?_wv=1027&k=PbxGByXj',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
